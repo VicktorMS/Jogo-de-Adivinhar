d02d2ff5289fe6d51317418675b16616e28cdc3a
Victor <Victor@DESKTOP-JI227NH>
2023-08-10 17:55:41 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo-de-adivinhar/Program.cs
@@ -0,0 +1,30 @@
+﻿
+void Main()
+{
+    Console.WriteLine("Bem-vindo ao jogo de adivinhação\n");
+   
+    
+    Random rnd = new Random();
+    int randomNumber = rnd.Next(1, 100);
+
+    int counter = 3;
+
+    do
+    {
+        Console.Write("Você tem {0} tentativas!\nInsira um número entre 1 e 100: ", counter);
+        string userNumberString = Console.ReadLine();
+        int userNumber = int.Parse(userNumberString);
+
+        if (userNumber == randomNumber)
+        {
+            Console.WriteLine($"Você ganhou!, o número era {randomNumber}!");
+            return;
+        }
+
+        counter--;
+    } while (counter > 0);
+    Console.WriteLine("Você perdeu, o número era: " + randomNumber);
+
+}
+
+Main();
